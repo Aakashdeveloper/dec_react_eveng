e22136fcdf7dcd171dd859883259b230e7d3fb2f
aakashdeveloper <ahanda205@gmail.com>
2020-12-17 22:40:22 +0530

    a

--- react1/src/index.js
+++ react1/src/index.js
@@ -1,17 +1,18 @@
+//var React = require('root');
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Header from './Header';
+import Footer from './Footer'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => { 
+    return(
+        <div>
+            <Header/>
+            <h1>My React Application</h1>
+            <h2>Developer Funnel</h2>
+            <Footer/>
+        </div>
+    )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App/>,document.getElementById('root'));
